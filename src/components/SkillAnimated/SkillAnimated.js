d91bd50dd8407bf29c75febd86660c5b69a804b9
robel16 <robelmuluwowrk@gmail.com>
2023-08-17 17:43:33 -0700

    animation added to the skill component

--- src/components/SkillAnimated/SkillAnimated.js
+++ src/components/SkillAnimated/SkillAnimated.js
@@ -1,22 +1,22 @@
 import { faCss3, faGitAlt, faHtml5, faJsSquare, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import './index.scss'
 
 const SkillAnimated=()=> {
   return (
     <div className='stage-cube-cont'>
         <div className='cubespinner'>
             <div className='face1'>
-                <FontAwesomeIcon icon={faReact} color="#5ED4F4"/>
+                <FontAwesomeIcon icon={faPython} color="#4B8BBE"/>
             </div>
             <div className='face2'>
-                <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
+                <FontAwesomeIcon  icon={faHtml5} color="#F06529"/>
             </div>
             <div className='face3'>
-                <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
+                <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
             </div>
             <div className='face4'>
-                <FontAwesomeIcon icon={faPython} color="#4B8BBE"/>
+                <FontAwesomeIcon  icon={faReact} color="#5ED4F4"/>
             </div>
             <div className='face5'>
                 <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
